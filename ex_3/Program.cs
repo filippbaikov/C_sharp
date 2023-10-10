97a969f90ae4f064ea2d82e98d634cb9f4a3054a
filipp <zeliboba0@bk.ru>
2023-10-10 22:18:31 +0300

    3 work

---
+++ ex_3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+int xa=40, ya=1;
+int xb=1, yb=30;
+int xc=80, yc=30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("#");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("#");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("#");
+
+int x=xa;
+int y=ya;
+int count=0;
+
+while(count<10000){
+    int randomMath= new Random().Next(0,3);
+    if (randomMath==0){
+        x=(x+xa)/2;
+        y=(y+ya)/2;
+    }
+     if (randomMath==1){
+        x=(x+xb)/2;
+        y=(y+yb)/2;
+    }
+     if (randomMath==2){
+        x=(x+xc)/2;
+        y=(y+yc)/2;
+    }
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine("#");
+    count++;
+    }
+
+
+
+
---
+++ example_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Console.SetCursorPosition(1, 1);
+//Console.WriteLine("#");
+
+int xa=1, ya=1;
+int xb=1, yb=30;
+int xc=40, yc=30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("#");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("#");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("#");
